1f0510ebc9d5ff9314e3acbdac9103a6303872c6
chaimaa-ess <65967906+chaimaa-ess@users.noreply.github.com>
2023-08-09 14:26:41 +0100

    Update ModuleMarket.py

--- src/oplem/ModuleMarket.py
+++ src/oplem/ModuleMarket.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Project  Module 
+OPLEM Market module has two types of markets:
 
-This module helps debug the documentation issue with readthedocs
+(1) Energy markets, and 
+
+(2) Flexibility markets
+
+(1) The energy market comes with subclasses of the common types of energy markets:
+
+(i) central market
+
+(ii) time of use market
+
+(iii) P2P market
+
+(iiii) auction market
+
+(2) The flexibility markets comes with one market
+
+(i) Capacity limits market
 """
 
 
