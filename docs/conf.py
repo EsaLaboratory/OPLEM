813199066251124bd3c5b99b045a91800eece9f9
chaimaa-ess <65967906+chaimaa-ess@users.noreply.github.com>
2023-09-11 22:15:53 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 #sys.path.append(os.path.abspath('../..'))
-sys.path.insert(1, os.path.abspath('../../'))
+sys.path.insert(1, os.path.abspath('../src/'))
 sys.path.insert(2, os.path.abspath('../src/oplem'))
 
 # -- Project information -----------------------------------------------------
